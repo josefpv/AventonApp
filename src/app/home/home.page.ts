c660371b5243f0a9e3bd0a5accb7017a1fea391e
Jose Vivas <vivas.f13@gmail.com>
2023-09-14 10:40:33 -0300

    Creacion de pantalla de login.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { GoogleMap } from '@capacitor/google-maps';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
-
 }
